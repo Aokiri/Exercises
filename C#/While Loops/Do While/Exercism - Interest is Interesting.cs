e61d258952870dfeb14357300e897504a150c18b
Allan D. <alandor95@hotmail.com>
2025-04-03 09:42:14 +0200

    Added more C# exercises

--- C#/While Loops/Do While/Exercism - Interest is Interesting.cs
+++ C#/While Loops/Do While/Exercism - Interest is Interesting.cs
@@ -40,14 +40,3 @@ static class SavingsAccount
         return years;
     }
 }
-
-
-using System;
-
-public static class ProteinTranslation
-{
-    public static string[] Proteins(string strand)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
-}
---
+++ C#/While Loops/Exercism - Protein Translation.cs
@@ -0,0 +1,11 @@
+// Link to the exercise: https://exercism.org/tracks/csharp/exercises/protein-translation
+
+using System;
+
+public static class ProteinTranslation
+{
+    public static string[] Proteins(string strand)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
